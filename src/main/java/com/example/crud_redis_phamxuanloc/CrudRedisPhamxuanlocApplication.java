2ca9385e3943f812e9ae1532c88bce6ffbfd3a66
phamxuanloc0 <locpham2307@gmail.com>
2021-11-05 07:40:22 +0700

    create project

---
+++ src/main/java/com/example/crud_redis_phamxuanloc/CrudRedisPhamxuanlocApplication.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_phamxuanloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRedisPhamxuanlocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRedisPhamxuanlocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crud_redis_phamxuanloc/CrudRedisPhamxuanlocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crud_redis_phamxuanloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRedisPhamxuanlocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
