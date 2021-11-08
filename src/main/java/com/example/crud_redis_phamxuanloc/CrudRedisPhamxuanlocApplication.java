13296c6553fecf430e399ef4dfe9a824660ce85b
phamxuanloc0 <locpham2307@gmail.com>
2021-11-08 11:02:13 +0700

    add redis_CRUD(hash)

--- src/main/java/com/example/crud_redis_phamxuanloc/CrudRedisPhamxuanlocApplication.java
+++ src/main/java/com/example/crud_redis_phamxuanloc/CrudRedisPhamxuanlocApplication.java
@@ -2,6 +2,9 @@ package com.example.crud_redis_phamxuanloc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class CrudRedisPhamxuanlocApplication {
@@ -9,5 +12,15 @@ public class CrudRedisPhamxuanlocApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrudRedisPhamxuanlocApplication.class, args);
     }
+    @Bean
+    LettuceConnectionFactory jedisConnectionFactory(){
 
+        return new LettuceConnectionFactory();
+    }
+    @Bean
+    RedisTemplate redisTemplate(){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        return redisTemplate;
+    }
 }
